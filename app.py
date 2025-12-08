59cf1c63eedd90d85a9e95f07f2e5d98f1b5e3b9
aalasaliano <aliyahjasmine05@gmail.com>
2025-12-08 18:18:28 +0700

    Update image path in get_base64_image function

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-img_base64 = get_base64_image("flower wallpaper 🌺.jpg")
+img_base64 = get_base64_image("annie-spratt-XibTbQFqIjY-unsplash.jpg")
 
 st.markdown(f"""
     <style>
