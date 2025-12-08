794b5a2ce8ca5226ca55b9b5c3e425e3bbe7441f
aalasaliano <aliyahjasmine05@gmail.com>
2025-12-08 18:43:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-img_base64 = get_base64_image("unsplash2.jpg")
+img_base64 = get_base64_image("stbackgroundz.jpg")
 
 st.markdown(f"""
     <style>
