b0d88dc93a1c9b76d26a48f70a02c3f6e8bc872c
aalasaliano <aliyahjasmine05@gmail.com>
2025-12-08 18:13:23 +0700

    Change image source to 'flower wallpaper 🌺.jpg'

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-img_base64 = get_base64_image("backgroundz2.jpg")
+img_base64 = get_base64_image("flower wallpaper 🌺.jpg")
 
 st.markdown(f"""
     <style>
